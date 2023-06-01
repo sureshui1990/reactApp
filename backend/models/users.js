6adcb29351996e538677b28d6fc3e3fbc7e8ecea
Suresh B <sureshui1990@gmail.com>
2023-06-01 12:24:36 +0530

    format modification and console removed

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // define models
 const userSchema = new Schema({
-    email: { type: String, lowercase: true, unique: true},
-    password: { type: String}
+  email: { type: String, lowercase: true, unique: true },
+  password: { type: String },
 });
 
-// create class 
-const ModelClass = mongoose.model('user', userSchema);
+// create class
+const ModelClass = mongoose.model("user", userSchema);
 
 // export
 module.exports = ModelClass;
-
--- backend/route.js
+++ backend/route.js
@@ -1,18 +1,18 @@
-const passport = require('passport');
-const Authentication = require('./controllers/authentication');
-const passportService = require('./services/passport');
+const passport = require("passport");
+const Authentication = require("./controllers/authentication");
+const passportService = require("./services/passport");
 
-const requireAuth = passport.authenticate('jwt', { session: false });
-const requireSignin = passport.authenticate('local', {session: false });
+const requireAuth = passport.authenticate("jwt", { session: false });
+const requireSignin = passport.authenticate("local", { session: false });
 
 const route = (app) => {
-    app.get('/', requireAuth, (req, res) => {
-        res.send({content: "hi there"});
-    });
+  app.get("/", requireAuth, (req, res) => {
+    res.send({ content: "hi there" });
+  });
 
-    app.post('/signin',requireSignin, Authentication.signin);
+  app.post("/signin", requireSignin, Authentication.signin);
 
-    app.post('/signup', Authentication.signup);
+  app.post("/signup", Authentication.signup);
 };
 
 module.exports = route;
